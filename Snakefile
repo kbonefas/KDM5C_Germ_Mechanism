47ca490add46b863e048632208392acbebba68b1
Katie Bonefas <kbonefas@umich.edu>
2024-04-12 16:12:06 -0400

    amy hip tissue specific genes

--- Snakefile
+++ Snakefile
@@ -5,6 +5,8 @@
 #                                                      #
 ########################################################
 
+####################### Figure 1 #######################
+
 ##padj cutoff for DESEq2
 PADJ = 0.1
 ##number of genotypes you're comparing to WT
@@ -30,3 +32,25 @@ rule amyhipDESeq2:
 		"results/figure_pieces/GOdot_hip.pdf"
 	script:
 		"code/DESeq2_adultHIPAMY.R"
+
+
+#look at tissue-enriched gene expression
+rule tissue_genes:
+	input:
+		"results/DESeq2/DEGs_amy5cKO.csv",
+		"results/DESeq2/DEGs_hip5cKO.csv",
+		"data/processed/restable_amy5cKO.csv",
+		"data/processed/restable_hip5cKO.csv"
+
+	output:
+		"results/figure_pieces/TissueSpecific_Volcano_amy5cKO.png",
+		"results/figure_pieces/TissueSpecific_Volcano_hip5cKO.png",
+		"results/figure_pieces/TissueSpecific_bar_amy5cKO.pdf",
+		"results/figure_pieces/TissueSpecific_bar_hip5cKO.pdf",
+		"results/figure_pieces/TissueSpecific_MAplot_amy5cKO.pdf",
+		"results/figure_pieces/TissueSpecific_MAplot_hip5cKO.pdf",
+		"data/processed/TissueSpecific_AMYHIP_numberofgenesDEGs.csv",
+		"data/processed/TestisDEGs_amyhip.csv",
+		"results/figure_pieces/TissueSpecific_bar_numbgenes.pdf"
+	script:
+		"code/TissueGenes_AmyHip.R"
---
+++ code/TissueGenes_AmyHip.R
@@ -0,0 +1,54 @@
+#moved calculation of tissue specific genes for brain here to make the code more robust
+source("code/utilities/tissueSPECIFICgenes.R") #read in the functions
+
+
+### run for the samples, make sure order matches snakefile
+titles <- c("Amygdala", "Hippocampus")
+fisherdfs <- list()
+# get the testis-specific DEGs for each samples for later plotting in wt and germline-depleted testis
+testisDEGs <- data.frame()
+for(i in 1:length(titles)){
+	tissDEG <- tissue_genes(i)
+	#testis DEGs
+	TEdeg <- subset(tissDEG, tissue == "Testis")
+	TEdeg <- subset(TEdeg, select = c("ENSEMBL", "SYMBOL"))
+	testisDEGs <- rbind(testisDEGs, TEdeg)
+
+	#n = position in snakefile for input DEG table and output volcano plot
+	#title = name of the dataset for the graph
+	#td = tissue-specific DEG 
+	#offset - number of samples to shift the input and output numbers to align correctly
+	#bary - y-axis of bar graph
+	tissue_plots(i, titles[i], tissDEG, length(titles), 50)
+
+
+	#calculate the enrichment
+	fisherdfs[[i]] <- tissue_fisher(tissDEG, i, titles[i])
+
+}
+
+
+#save the calculations of tissue number and enrichment
+
+#merge together the tissue count with the fisher results
+fisherdfs[[3]] <- countingdf
+library(tidyverse)
+fisherdfs <- fisherdfs %>% reduce(full_join, by = "tissue")
+print(fisherdfs)
+
+write.table(fisherdfs, snakemake@output[[7]], sep = ",", row.names = FALSE)
+
+#saving the testis DEGs
+testisDEGs <- unique(testisDEGs)
+print(head(testisDEGs))
+write.table(testisDEGs, snakemake@output[[8]], sep = ",", row.names = FALSE)
+
+
+
+### make a histogram of the total number of tissue specific genes
+p <- ggbarplot(countingdf, "tissue", "count", fill = "tissue", color = "tissue", palette = tissue_palette, 
+		label = TRUE, lab.pos = "out", lab.col = "black") + rotate_x_text(45) 
+
+
+pl <- ggpar(p, legend = "none", ylab = "# of genes", xlab = FALSE, title = paste0("Total Tissue Gene #") )
+ggsave(snakemake@output[[9]], plot = pl, width = 5, height = 5)
