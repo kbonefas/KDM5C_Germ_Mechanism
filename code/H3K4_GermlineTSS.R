3c97f6dbdb34431c0f6e43ddac3b4bf2a9005669
Katie Bonefas <kbonefas@umich.edu>
2024-06-28 09:17:14 -0400

    update gene coordinates from bioMart are wrong

--- code/H3K4_GermlineTSS.R
+++ code/H3K4_GermlineTSS.R
@@ -9,30 +9,33 @@ mart <- useMart("ensembl", dataset = "mmusculus_gene_ensembl")
 #get the list of germline genes
 germ <- read.csv(snakemake@input[[1]], sep = ",") 
 
-#genelist - list of gene symbols you want the window for
+#genelist - list of ensembl gene IDs you want the window for
 #window - # of bp upstream and downstream
 #out - output location in snakemake for bedfile
 TSSwindowBED <- function(genelist, window, out){
-	#attributes = listAttributes(mart, page = "structure")
+	#listAttributes(mart, page = "structure")
 	#get the TSS for list of genes
 	tss <- getBM(attributes = c("chromosome_name", "transcription_start_site", "transcript_end",
-                            "ensembl_gene_id", "ensembl_transcript_id", "external_gene_name"), filters = "external_gene_name", values = genelist, mart = mart)
+                            "ensembl_gene_id", "start_position", "end_position", "ensembl_transcript_id", "external_gene_name"), filters = "ensembl_gene_id", values = genelist, mart = mart)
 
 	#add and subtract bp window and format in bed style
-	bed <- data.frame(chr = paste0("chr", tss$chromosome_name), start = tss$transcription_start_site - window, end =  tss$transcription_start_site + window)
+	print(tss)
+	bed <- unique(data.frame(chr = paste0("chr", tss$chromosome_name), start = tss$transcription_start_site - window, end =  tss$transcription_start_site + window))
+	print(bed)
 	write.table(bed, snakemake@output[[out]], sep = "\t", row.names = FALSE, col.names=FALSE, quote = FALSE)
 
-	bed2 <- data.frame(chr = paste0("chr", tss$chromosome_name), start = tss$transcription_start_site, end =  tss$transcript_end)
+	bed2 <- unique(data.frame(chr = paste0("chr", tss$chromosome_name), start = tss$start_position, end =  tss$end_position))
+	print(bed2)
 	write.table(bed2, snakemake@output[[out+1]], sep = "\t", row.names = FALSE, col.names=FALSE, quote = FALSE)
 
 }
 
 	
 WIND <- 1000
-TSSwindowBED(germ$SYMBOL, WIND, 1)
-testgenes <- c("Dazl", "Cyct", "D1Pas1", "Ddx4")
+#TSSwindowBED(germ$ENSEMBL, WIND, 1)
+testgenes <- c("ENSMUSG00000010592", "ENSMUSG00000056436", "ENSMUSG00000021758")
 
-TSSwindowBED(testgenes, WIND, 3)
+TSSwindowBED(testgenes, WIND, 1)
 
 
 #just the brain or EpiLC DEGs
@@ -40,14 +43,14 @@ samples <- c("EpiLC", "Amygdala", "Hippocampus")
 allDEGs <- c()
 for (i in 1:length(samples)){
 	germDEGs <- read.csv(snakemake@input[[i+1]], sep = ",")
-	allDEGs <- c(allDEGs, germDEGs$SYMBOL)
+	allDEGs <- c(allDEGs, germDEGs$ENSEMBL)
 }
 
 allDEGs <- unique(allDEGs)
 print(allDEGs[1:10])
 print(length(allDEGs))
 
-TSSwindowBED(allDEGs, WIND, 5)
+#TSSwindowBED(allDEGs, WIND, 5)
 
 
 
