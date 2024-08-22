1bc67044ed0dc678b9c4bb5a90918aa4e592268f
Bonefas <kbonefas@med.umich.edu>
2024-08-22 18:16:50 -0400

    writing heatmap part of script

--- code/serverscripts/WGBS/deeptools_heatmap.py
+++ code/serverscripts/WGBS/deeptools_heatmap.py
@@ -28,37 +28,34 @@ for g in genotypes:
 avgbigwigs = glob.glob("../results/bedgraph/avg_exEpiLC*.bw")
 
 #bed file coordinates
-regions = ["TSS", "CGI"]
+germ_regions = ["TSS", "CGI"]
 
 #wrap all the deeptools fucntions together
 #regions - gene coordinates
 #bigwigs - bigwigs you're plotting
 #colors - color for heatmap
-def deeptoolswrap(regions, bigwigs, name):
+def deeptoolswrap(regions, bigwigs):
 	for i in regions:
 
 		#get the bedfiles of that region
-		beds = glob.glob("../data/processed/{}")
+		beds = glob.glob("../data/processed/*{reg}*.bed".format(reg = i))
+		beds = beds.join(" ")
 
-		#name of the output - ID is the bigwig id, coord is the bed cooridnates
-		coord = i.replace("../data/processed/GermTSSandTES/TSS_window_", "")
-		coord = coord.replace(".bed", "")
-		
-		window = 3000
-		out = "../results/plotProfile_TSS_KDM5C_{NAME}_{COORD}".format(COORD = coord, NAME = name)
+		#name of the output 
+		out = "../results/deeptools/plotProfile_WGBS_EpiLC_WTvsKO_{reg}".format(reg = i)
 		
 		#make the compute matrix
-		os.system('computeMatrix reference-point -S {BIGWIGS} -R {REGIONS} --referencePoint center -a {WINDOW} -b {WINDOW} -o {OUT}_matrix.mat.gz'.format(BIGWIGS = " ".join(bigwigs), REGIONS = i, WINDOW = window, OUT = out))
-		title = "KDM5C_{COORD}".format(COORD = coord)
+		os.system('computeMatrix scale-regions -S {BIGWIGS} -R {REGIONS} -o {OUT}_matrix.mat.gz'.format(BIGWIGS = " ".join(bigwigs), REGIONS = beds, OUT = out))
+		title = "Germline_{reg}".format(reg = i)
 		
 		#make the average plot --perGroup splits by bedfile so all bigwigs are plotted on same graph
-		os.system('plotProfile -m {OUT}_matrix.mat.gz -out {OUT}_profile.pdf --perGroup --refPointLabel TSS --samplesLabel Kdm5c_WT Kdm5c_KO --plotTitle {TITLE} --plotWidth 10 --plotHeight 10 --plotFileFormat pdf'.format(OUT = out, TITLE = title))
+		os.system('plotProfile -m {OUT}_matrix.mat.gz -out {OUT}_profile.pdf --perGroup --startLabel start --endLabel end --samplesLabel Kdm5c_KO Kdm5c_WT --plotTitle {TITLE} --plotWidth 10 --plotHeight 10 --plotFileFormat pdf'.format(OUT = out, TITLE = title))
 		
 		#make heatmap plot
-		os.system('plotHeatmap -m {OUT}_matrix.mat.gz -out {OUT}_heatmap.pdf --perGroup --yMax 40 --zMax 60 --colorMap RdPu --refPointLabel TSS --missingDataColor white --samplesLabel Kdm5c_WT Kdm5c_KO --plotTitle {TITLE} --heatmapHeight 14 --heatmapWidth 5 --plotFileFormat pdf'.format(OUT = out, TITLE = title))
+		#--yMax 40 --zMax 60
+		os.system('plotHeatmap -m {OUT}_matrix.mat.gz -out {OUT}_heatmap.pdf --perGroup --colorMap RdPu --startLabel start --endLabel end --missingDataColor white --samplesLabel Kdm5c_KO Kdm5c_WT --plotTitle {TITLE} --heatmapHeight 14 --heatmapWidth 5 --plotFileFormat pdf'.format(OUT = out, TITLE = title))
 		
 		#os.system("plotHeatmap -m {OUT}_matrix.mat.gz --colorList 'white, #63344c' 'white, #63344c' 'white, #275c62' 'white, #275c62' --missingDataColor white --heatmapHeight 14 --heatmapWidth 5 -out {OUT}_heatmap.pdf".format(MATRIX = MATRIX, NAME = NAME))
 
 
-deeptoolswrap(germregions, EpiLC, 'EpiLC')
-deeptoolswrap(germregions, PNC, 'PNC')
+deeptoolswrap(germ_regions, avgbigwigs)
