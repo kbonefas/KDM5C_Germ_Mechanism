299bdd321a47a1f764d7b9346f8120f53a4246e2
Bonefas <kbonefas@med.umich.edu>
2024-08-22 17:21:20 -0400

    bedgraph to bigwig code

--- code/serverscripts/WGBS/bedgraph_to_bigwig.py
+++ code/serverscripts/WGBS/bedgraph_to_bigwig.py
@@ -9,26 +9,23 @@ import glob
 
 #convert bedraphs files into bigwig files
 	# get list of bedgraph files
-begraph = glob.glob("../results/bedgraph/*.deduplicated.bedGraph.gz")
+bedgraph = glob.glob("../results/bedgraph/*.deduplicated.bedGraph.gz")
 	#unzip the bedgraph file
 
 for i in bedgraph:
-	#name of unzipped
-	unzipped = i.replace(".gz", "")
 	#name of file without extensions
-	clean = unzipped.replace(".bedGraph", "")
+	clean = i.replace(".bedGraph.gz", "")
 
 	print("unzipping bedgraph for {sample}".format(sample = clean))
-	#unzip the file
-	os.system("nohup nice 10 gzip -d {file} &".format(file = i))
+	# #unzip the file
+	os.system("nice gzip -d {file}".format(file = i))
 
+unzipped = glob.glob("../results/bedgraph/*.deduplicated.bedGraph")
+
+for u in unzipped:
+	clean = u.replace(".bedGraph", "")
 	#make the bigwig
 	print("making bigwig for {sample}".format(sample = clean))
-	os.system("nohup nice 10 bedGraphToBigWig {inbedgraph} /nfs/value/siwase2/GENOMES/MM10/mm10.chrom.sizes {clean}.bw &".format(inbedgraph = unzipped, clean = clean))
-
-	#rezip the file
-	print("rezipping {sample}".format(sample = clean))
-	os.system("nohup nice 10 gzip {file} &".format(file = unzipped))
-
-
+	os.system("nice bedGraphToBigWig {inbedgraph} /nfs/value/siwase2/GENOMES/MM10/mm10.chrom.sizes {clean}.bw".format(inbedgraph = u, clean = clean))
+	
 
