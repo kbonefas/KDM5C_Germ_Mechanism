b3209348e024f0501984717bb7a3cebcc168127d
Katie Bonefas <kmb395@cornell.edu>
2025-03-18 18:01:27 -0400

    change to barplot

--- code/RA_ICCplots.R
+++ code/RA_ICCplots.R
@@ -16,11 +16,11 @@ print(head(DAZL_100nMRA))
 
 my_comparisons <- list(c("WT RA", "5CKO RA"), c("WT DMSO", "5CKO DMSO"), c("5CKO DMSO", "5CKO RA"), c("WT DMSO", "WT RA"))
 
-q <- ggboxplot(DAZL_100nMRA, x = 'genotreat', y = 'Percent', color = "black", fill="Genotype",
-    add.params = list(size = 1), add =  "dotplot", xlab = " ") + #palette = EpiLC_XY_palette
+q <- ggbarplot(DAZL_100nMRA, x = 'genotreat', y = 'Percent', fill="genotreat", color = "genotreat", add = "mean_se",
+    xlab = " ", ylab = "% DAZL+/DAPI+") + #palette = EpiLC_XY_palette
     #rremove("legend") +
     stat_compare_means(comparisons = my_comparisons, method="t.test", label = "p.signif") 
-q <- ggpar(q, x.text.angle = 25, font.main = "bold") #ylim = c(0,40),
+q <- ggpar(q, x.text.angle = 25, font.main = "bold")
 
 # q <- facet(q, facet.by = "Symbol", nrow = 2)
 
