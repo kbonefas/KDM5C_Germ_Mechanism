a74197a32ecc6ae5cc0b3cae1700bd3957306c5e
Katie Bonefas <kmb395@cornell.edu>
2025-09-01 11:49:29 -0400

    gene ontology comparison

--- code/GO_Compare_BrainEpiLC.R
+++ code/GO_Compare_BrainEpiLC.R
@@ -7,6 +7,8 @@ library(ggplot2)
 
 # make a list with all the germline genes
 germ <- read.csv(snakemake@input[[1]], sep = ",", header = TRUE)
+print("germ")
+print(head(germ))
 
 #sample names, make sure order matches snakefile input
 samples <- c("nESC", "EpiLC", "exEpiLC", "Amygdala", "Hippocampus")
@@ -14,15 +16,16 @@ germDEGs <- list()
 
 for(i in 1:length(samples)){
 	DEGs <- read.csv(snakemake@input[[i+1]], sep = ",", header = TRUE)
+	print(head(DEGs))
 
 	#subset for germline DEGs
-	germDEGs <- subset(DEGs, DEGs$ENSEMBL %in% germ$ENSEMBL)
+	germDEGs1 <- subset(DEGs, DEGs$ENSEMBL %in% germ$ENSEMBL)
 	
 	#save the germ DEGs
-	write.table(snakemake@output[[i]], sep = ",", row.names = FALSE)
+	write.table(germDEGs1, snakemake@output[[i]], sep = ",", row.names = FALSE)
 
 	#get the ensembl names, put in the position of the list
-	germDEGs[[i]] <- DEGs[,1]
+	germDEGs[[i]] <- germDEGs1[,1]
 }
 
 names(germDEGs) <- samples
@@ -31,7 +34,7 @@ print(head(germDEGs))
 #now run the gene ontology comparison
 ck <- compareCluster(geneCluster = germDEGs, fun = enrichGO,  OrgDb = "org.Mm.eg.db", keyType="ENSEMBL", ont="BP")
 #ck <- setReadable(ck, OrgDb = "org.Mm.eg.db", keyType="ENSEMBL")
-head(ck) 
+head(ck)
 
 write.table(ck, snakemake@output[[length(samples) + 1]], row.names = FALSE, sep = ",")
 
