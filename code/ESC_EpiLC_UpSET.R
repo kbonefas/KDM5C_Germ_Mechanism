b487ef30d50113a72d2d4ba3dc9cb710a3ff1f10
Katie Bonefas <kmb395@cornell.edu>
2026-01-15 22:24:37 -0500

    lowercase

--- code/ESC_EpiLC_UpSET.R
+++ code/ESC_EpiLC_UpSET.R
@@ -25,7 +25,7 @@ modifiedupset <- function(samplelist){
 
 pdf(file = snakemake@output[[1]], width = 8, height = 5)
 
-upset(fromList(DEGs), order.by = "freq",  sets.x.label = "# Germline DEGs", mainbar.y.label = "# in Group", text.scale = 2, sets = samples, mb.ratio = c(0.55, 0.45), keep.order = TRUE)
+upset(fromList(DEGs), order.by = "freq",  sets.x.label = "# germline DEGs", mainbar.y.label = "# in group", text.scale = 2, sets = samples, mb.ratio = c(0.55, 0.45), keep.order = TRUE)
 
 dev.off()
 
