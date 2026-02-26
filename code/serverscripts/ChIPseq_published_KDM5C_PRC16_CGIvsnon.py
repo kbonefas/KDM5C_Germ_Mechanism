103bbb7c8c152b400387e4ad84b2d635070a0ede
Katie Bonefas <kmb395@cornell.edu>
2026-02-25 20:33:17 -0500

    split histone marks and binding proteins

--- code/serverscripts/ChIPseq_published_KDM5C_PRC16_CGIvsnon.py
+++ code/serverscripts/ChIPseq_published_KDM5C_PRC16_CGIvsnon.py
@@ -49,9 +49,15 @@ import glob
 
 
 #### Also do all of the marks together so they're organized at the same regions
-command = 'computeMatrix reference-point -S Bigwig_Analysis/Agarwal_KDM5C_rep1.bw Bigwig_Analysis/Agarwal_KDM5C_rep2.bw Bigwig_Analysis/Stielow_MGA.bw Bigwig_Analysis/Stielow_PCGF6.bw Bigwig_Analysis/Blackledge_H2AK119ub1.bw Bigwig_Analysis/Blackledge_H3K27me3.bw Bigwig_Analysis/ENCODE_H3K9me3.bw -R TSS_germ_CGI.bed TSS_germ_CGIfree.bed --referencePoint center -a {WINDOW} -b {WINDOW} -o ESC_KDM5C_PRC16_germ_matrix_2.mat.gz'.format(WINDOW = 3000)
+command = 'computeMatrix reference-point -S Bigwig_Analysis/Agarwal_KDM5C_rep1.bw Bigwig_Analysis/Agarwal_KDM5C_rep2.bw Bigwig_Analysis/Stielow_MGA.bw Bigwig_Analysis/Stielow_PCGF6.bw -R TSS_germ_CGI.bed TSS_germ_CGIfree.bed --referencePoint center -a {WINDOW} -b {WINDOW} -o ESC_KDM5C_PRC16_germ_matrix_2.mat.gz'.format(WINDOW = 3000)
 
 os.system(command)
-os.system("plotHeatmap -m ESC_KDM5C_PRC16_germ_matrix_2.mat.gz -out ESC_KDM5C_PRC16_germ_heatmap_CGIsplit.pdf --colorMap RdPu --refPointLabel TSS --missingDataColor white --plotFileFormat pdf  --heatmapHeight 20 --heatmapWidth 4")
+os.system("plotHeatmap -m ESC_KDM5C_PRC16_germ_matrix_2.mat.gz -out ESC_germ_KDM5C_TFs_CGIsplit.pdf --colorMap RdPu --refPointLabel TSS --missingDataColor white --plotFileFormat pdf  --heatmapHeight 20 --heatmapWidth 4")
+
+
+command = 'computeMatrix reference-point -S Bigwig_Analysis/Blackledge_H2AK119ub1.bw Bigwig_Analysis/Blackledge_H3K27me3.bw Bigwig_Analysis/ENCODE_H3K9me3.bw -R TSS_germ_CGI.bed TSS_germ_CGIfree.bed --referencePoint center -a {WINDOW} -b {WINDOW} -o ESC_KDM5C_PRC16_germ_matrix_3.mat.gz'.format(WINDOW = 3000)
+
+os.system(command)
+os.system("plotHeatmap -m ESC_KDM5C_PRC16_germ_matrix_3.mat.gz -out ESC_germ_histone_CGIsplit.pdf --colorMap RdPu --refPointLabel TSS --missingDataColor white --plotFileFormat pdf  --heatmapHeight 20 --heatmapWidth 4")
 
 
