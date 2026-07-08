917fa4d8a90006979166015204dc166f873d2dcd
Katie Bonefas <kmb395@cornell.edu>
2026-07-07 20:59:48 -0400

    pie chart of germ DEGs

--- Snakefile
+++ Snakefile
@@ -166,7 +166,8 @@ rule GO_EpiLC_vs_Brain:
 		"results/GO_germgenes.csv",
 		"results/figure_pieces/GO_germgenes.pdf",
 		"results/figure_pieces/Upset_germDEGs.pdf",
-		"results/figure_pieces/GO_germgenes_simple.pdf"
+		"results/figure_pieces/GO_germgenes_simple.pdf",
+		"results/figure_pieces/Pie_germDEGs.pdf"
 	script:
 		"code/GO_Compare_BrainEpiLC.R"
 
--- code/GO_Compare_BrainEpiLC.R
+++ code/GO_Compare_BrainEpiLC.R
@@ -78,3 +78,40 @@ upset(fromList(germDEGs), order.by = "freq",  sets.x.label = "# Germline DEGs", 
 dev.off()
 
 
+# 2026.07.07 percentage of all germline genes that are DEGs at any point
+allgermDEGs<- unique(unlist(germDEGs))
+print(paste0("Number of germline genes that are DEGs at any point: ", length(allgermDEGs)))
+
+#subset germ genes that are not DEGs
+nonDEGs <- subset(germ, !(germ$ENSEMBL %in% allgermDEGs))
+print(paste0("Number of germline genes that are not DEGs at any point: ", nrow(nonDEGs)))
+
+print(paste0("Number of germline genes: ", nrow(germ)))
+
+
+library(ggplot2)
+
+# Create Data
+data <- data.frame(
+  group=c("DEG", "non-DEG"),
+  value=c(length(allgermDEGs)/nrow(germ), nrow(nonDEGs)/nrow(germ))
+)
+print(data)
+data$percent <- round(data$value * 100, 2)
+data$labels <- paste0(data$percent, "%")
+# Basic piechart
+source("code/utilities/colorpalettes.R")
+p <- ggplot(data, aes(x="", y=percent, fill=group)) +
+  geom_bar(stat="identity", width=1, color="white") +
+  coord_polar("y", start=0) +
+  
+  theme_void() + # remove background, grid, numeric labels
+
+  geom_text(aes(label = labels), position = position_stack(vjust = 0.5), color = "white") +
+  scale_fill_manual(values=c("DEG" = germcolor, "non-DEG" = "#194219"))
+
+pdf(file = snakemake@output[[10]], width = 5.5, height = 5)
+
+p
+
+dev.off()
